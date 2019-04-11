5d8d447a0b707471f17b69509f9e89a1f89d8926
Mauricio Jimenez <mauriciobenjamin@gmail.com>
2019-04-11 18:56:58 -0500

    Correcciones menores en el ultimo post

--- entregas.sh
+++ entregas.sh
@@ -1,1 +1,23 @@
 #!/bin/zsh
+#Actualización de la rama vigente
+gulp --production
+git add .
+git commit -m "$1"
+git push origin actualizacion
+
+#Sincronización de los archivos de producción
+rsync -rv ~/Desarrollo/Technodomus/segunda/src/ ~/Desarrollo/Technodomus/paginaweb/src/ 
+rsync -rv ~/Desarrollo/Technodomus/segunda/build/ ~/Desarrollo/Technodomus/originales/
+
+#Actualización del git maestro
+../paginaweb
+git add .
+git commit -m "$1"
+git push origin master
+
+#Actualización del git de la página
+../originales
+git add .
+git commit -m "$1"
+git push origin master
+
