255fd25d0794f805e5f6a11dd8208b8dad800c24
Mauricio Jimenez <mauriciobenjamin@gmail.com>
2019-04-16 19:32:06 -0500

    Eliminacion de link a pagina del Bajio

--- entregas.sh
+++ entregas.sh
@@ -9,15 +9,15 @@ git push origin actualizacion
 rsync -rv ~/Desarrollo/Technodomus/segunda/src/ ~/Desarrollo/Technodomus/paginaweb/src/ 
 rsync -rv ~/Desarrollo/Technodomus/segunda/build/ ~/Desarrollo/Technodomus/originales/
 
-#Actualización del git maestro
-../paginaweb
-git add .
-git commit -m "$1"
-git push origin master
+# #Actualización del git maestro
+# ../paginaweb
+# git add .
+# git commit -m "$1"
+# git push origin master
 
-#Actualización del git de la página
-../originales
-git add .
-git commit -m "$1"
-git push origin master
+# #Actualización del git de la página
+# ../originales
+# git add .
+# git commit -m "$1"
+# git push -u origin master
 
