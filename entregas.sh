cd98520f36f185f6d275dbad649367b26d0789b4
Mauricio Jimenez <mauriciobenjamin@gmail.com>
2019-11-30 13:48:17 -0600

    Correccion en el nombre del archivo de logo

--- entregas.sh
+++ entregas.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 #Actualización de la rama vigente
-gulp --production
 git add .
 git commit -m "$1"
 git push origin actualizacion
 
 #Sincronización de los archivos de producción
+gulp --production
 rsync -rv ~/Desarrollo/Technodomus/segunda/src/ ~/Desarrollo/Technodomus/paginaweb/src/ 
 rsync -rv ~/Desarrollo/Technodomus/segunda/build/ ~/Desarrollo/Technodomus/originales/
 
-ORIGINALES="/home/mauricio/Desarrollo/Technodomus/originales/"
+ORIGINALES=/home/mauricio/Desarrollo/Technodomus/originales/
 
 #Actualización del git de la página
 git -C $ORIGINALES add .
